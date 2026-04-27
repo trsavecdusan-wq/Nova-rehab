5dc9d3dbdd21c45c6c30bfe523a4a39467abb0a4
trsavecdusan-wq <trsavecdusan@gmail.com>
2026-04-27 20:13:15 +0100

    Update RehabCallExtras.kt

--- app/src/main/java/com/novarehab/ui/RehabCallExtras.kt
+++ app/src/main/java/com/novarehab/ui/RehabCallExtras.kt
@@ -1,1 +1,9 @@
+package com.novarehab.ui
+
+object RehabCallExtras {
+    const val EXTRA_CONTACT_NAME = "contactName"
+    const val EXTRA_PREFERRED_LANGUAGE_CODE = "preferredLanguageCode"
+    const val EXTRA_CONTACT_ID = "contactId"
+    const val EXTRA_ROOM_ID = "roomId"
+}
 
