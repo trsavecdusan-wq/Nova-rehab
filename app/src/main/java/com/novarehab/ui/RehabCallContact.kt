fb0102c639f3f5e608972dacdb0aa03b83bbc2bb
trsavecdusan-wq <trsavecdusan@gmail.com>
2026-04-27 20:12:45 +0100

    Update RehabCallContact.kt

--- app/src/main/java/com/novarehab/ui/RehabCallContact.kt
+++ app/src/main/java/com/novarehab/ui/RehabCallContact.kt
@@ -1,1 +1,12 @@
 
+package com.novarehab.ui
+
+import androidx.annotation.DrawableRes
+
+data class RehabCallContact(
+    val id: String,
+    val displayName: String,
+    @DrawableRes val imageRes: Int,
+    val preferredLanguageCode: String,
+    val callRoomId: String
+)
