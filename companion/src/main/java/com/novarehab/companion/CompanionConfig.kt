aa31e3c290a32d5b58f1a2b000fa7125cd7567ca
trsavecdusan-wq <trsavecdusan@gmail.com>
2026-04-27 23:17:03 +0100

    Create CompanionConfig.kt

---
+++ companion/src/main/java/com/novarehab/companion/CompanionConfig.kt
@@ -0,0 +1,8 @@
+package com.novarehab.companion
+
+object CompanionConfig {
+    const val companionId: String = "companion_placeholder_001"
+    const val contactName: String = "Družinski član"
+    const val roomId: String = "room_placeholder_001"
+    const val preferredLanguageCode: String = "sl"
+}
