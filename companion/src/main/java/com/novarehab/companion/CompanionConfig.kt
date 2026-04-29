b9b32505e478720853784b87ff585a1641fa5620
trsavecdusan-wq <trsavecdusan@gmail.com>
2026-04-29 19:30:04 +0100

    Update CompanionConfig.kt

--- companion/src/main/java/com/novarehab/companion/CompanionConfig.kt
+++ companion/src/main/java/com/novarehab/companion/CompanionConfig.kt
@@ -6,6 +6,7 @@ object CompanionConfig {
     val contactName: String = BuildConfig.CONTACT_NAME
     val roomId: String = BuildConfig.ROOM_ID
     val preferredLanguageCode: String = BuildConfig.PREFERRED_LANGUAGE_CODE
+    val signalingBaseUrl: String = BuildConfig.SIGNALING_BASE_URL
     val updateCheckUrl: String = BuildConfig.UPDATE_CHECK_URL
     val apkDownloadUrl: String = BuildConfig.APK_DOWNLOAD_URL
 }
